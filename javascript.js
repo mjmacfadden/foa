ea430449f846c9761b975020243523669175edfb
Mike Macfadden <mikemacfadden@gmail.com>
2020-12-15 11:01:34 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
 var down = document.getElementById('topic');
 
 var arr = [
+    //SEASON 01 EPISODE 01
     "<a href='https://www.elliptigo.com/products/elliptigo-3c/' target='_blank'>The Eliptigo C3</a><br><span>S01E01</span>",
     "Quarantine Haircuts<br><span>S01E01</span>",
     "Sourdough Breadmaking<br><span>S01E01</span>",
+    //SESAON 01 EPISODE 02
     "Bidets<br><span>S01E02</span>",
     "<a href='https://twitter.com/sageboggs/status/1242968530250870786?s=20' target='_blank'>Electricity Biscuits</a><br><span>S01E02</span>",
     "TikTok<br><span>S01E02</span>",
     "<a href='https://www.imdb.com/title/tt1462764/' target='_blank'>Indiana Jones 5</a><br><span>S01E02</span>",
+    //SESAON 01 EPISODE 0
     "<a href='https://super73.com/' target='_blank'>Super 73 Electric Bike<br><span>S01E03</span>",
     "Adults Who Drink Milk<br><span>S01E03</span>",
     "Self Driving Cars<br><span>S01E03</span>",
